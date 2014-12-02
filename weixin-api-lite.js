26e0a2e1084535ff53bbd63b44c61702eb126447
zhangdaiping <zhangdaiping@gmail.com>
2014-12-02 11:25:21 +0800

    优化代码

--- weixin-api-lite.js
+++ weixin-api-lite.js
@@ -42,11 +42,11 @@ window.WeixinAPI = (function() {
 
     function ready(data, callback) {
         var self = this;
+        wxData = data || {};
         if (!isReady) {
             var _wxBridgeReady = function () {
                 wxBridgeReady(self, callback);
             };
-            wxData = data || {};
             if ('addEventListener' in document) {
                 document.addEventListener('WeixinJSBridgeReady', _wxBridgeReady, false);
             } else if (document.attachEvent) {
